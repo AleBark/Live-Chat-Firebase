58721b57e9798694ac303678bbc00b8152cf5b4f
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-08-26 23:00:44 -0300

    starting sign in methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() async {
   runApp(MyApp());
@@ -17,14 +19,30 @@ final ThemeData defaultTheme = ThemeData(
   accentColor: Colors.orangeAccent[400],
 );
 
-class MyApp extends StatelessWidget {
+final googleSignIn = GoogleSignIn();
+final auth = FirebaseAuth.instance;
 
+_ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+
+  if (user == null) {
+    user = await googleSignIn.signInSilently();
+  }
+  if (user == null) {
+    user = await googleSignIn.signIn();
+  }
+}
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Live Chat Flutter',
-      theme: Theme.of(context).platform == TargetPlatform.iOS ? iOsTheme : defaultTheme,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? iOsTheme
+          : defaultTheme,
       home: ChatScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 
@@ -50,10 +68,25 @@ class _ChatScreenState extends State<ChatScreen> {
         appBar: AppBar(
           title: Text("Live Chat"),
           centerTitle: true,
-          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).cardColor,
@@ -73,7 +106,6 @@ class TextComposer extends StatefulWidget {
 }
 
 class _TextComposerState extends State<TextComposer> {
-
   bool _isComposing = true;
 
   @override
@@ -82,22 +114,21 @@ class _TextComposerState extends State<TextComposer> {
       data: IconThemeData(color: Theme.of(context).accentColor),
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: Theme.of(context).platform == TargetPlatform.iOS ?
-        BoxDecoration(
-          border: Border(top: BorderSide(color: Colors.grey[200]))
-        ) : null,
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
         child: Row(
           children: <Widget>[
             Container(
-              child: IconButton(
-                icon: Icon(Icons.photo_camera),
-                onPressed: (){},
-              )
-            ),
+                child: IconButton(
+              icon: Icon(Icons.photo_camera),
+              onPressed: () {},
+            )),
             Expanded(
               child: TextField(
                 decoration: InputDecoration.collapsed(hintText: "Send"),
-                onChanged: (text){
+                onChanged: (text) {
                   setState(() {
                     _isComposing = text.length > 0;
                   });
@@ -107,14 +138,14 @@ class _TextComposerState extends State<TextComposer> {
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 4.0),
               child: Theme.of(context).platform == TargetPlatform.iOS
-              ? CupertinoButton(
-                child: Text("Send"),
-                onPressed: _isComposing ? (){} : null,
-              ) :
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: _isComposing ? (){} : null,
-              ),
+                  ? CupertinoButton(
+                      child: Text("Send"),
+                      onPressed: _isComposing ? () {} : null,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isComposing ? () {} : null,
+                    ),
             )
           ],
         ),
@@ -123,3 +154,37 @@ class _TextComposerState extends State<TextComposer> {
   }
 }
 
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Ale",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("Test"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
