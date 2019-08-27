0c7257eee87c8d3b70f1c57350b192f0109fabc9
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-08-26 22:47:15 -0300

    Custom btns for each theme, icons logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
-
   runApp(MyApp());
 }
 
 final ThemeData iOsTheme = ThemeData(
   primarySwatch: Colors.orange,
   primaryColor: Colors.grey[100],
-  primaryColorBrightness: Brightness.light
+  primaryColorBrightness: Brightness.light,
 );
 
 final ThemeData defaultTheme = ThemeData(
-    primarySwatch: Colors.purple,
-    primaryColor: Colors.orange[400],
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
 );
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Live Chat Flutter',
-      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS ? iOsTheme : defaultTheme,
+      home: ChatScreen(),
     );
   }
 
@@ -31,6 +32,94 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return null;
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Live Chat"),
+          centerTitle: true,
+          elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+
+  bool _isComposing = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS ?
+        BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey[200]))
+        ) : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: (){},
+              )
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: "Send"),
+                onChanged: (text){
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+              ? CupertinoButton(
+                child: Text("Send"),
+                onPressed: _isComposing ? (){} : null,
+              ) :
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: _isComposing ? (){} : null,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
